4d33bbcddf09ef6a8e39fe259e1323c1896a8f88
igor <tk.igor@gmail.com>
2021-03-03 19:50:55 +0300

    add pathbase prefix

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -14,21 +15,43 @@ func getdtHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, time.Now().Format(time.RFC3339))
 }
 
-func startServer(addr string) {
+func normalizePathBase(pathBase string) string {
+	if pathBase == "" {
+		return pathBase
+	}
+
+	if strings.HasPrefix(pathBase, "/") == false {
+		pathBase = strings.TrimLeft(pathBase, "/")
+	}
+
+	if strings.HasSuffix(pathBase, "/") {
+		pathBase = strings.TrimRight(pathBase, "/")
+	}
+
+	pathBase = "/" + pathBase
+	return pathBase
+}
+
+func startServer(addr string, pathBase string) {
 	router := mux.NewRouter()
-	router.HandleFunc("/tests/get-dt", getdtHandler)
-	http.Handle("/", router)
 
-	fmt.Printf("Server is listening at: http://%s ...", addr)
+	app := router.PathPrefix(pathBase).Subrouter()
+	app.HandleFunc("/tests/get-dt", getdtHandler)
 
-	http.ListenAndServe(addr, nil)
+	fmt.Printf("Server is listening at: http://%s%s ...", addr, pathBase)
+
+	http.ListenAndServe(addr, router)
 }
 
 func main() {
 	var portPtr = flag.Int("port", 8181, "TCP/IP port")
+	var pathBasePtr = flag.String("pathbase", "", "Path base")
+
 	flag.Parse()
+
+	var pathBase = normalizePathBase(*pathBasePtr)
 
 	addr := "0.0.0.0:" + strconv.Itoa(*portPtr)
 
-	startServer(addr)
+	startServer(addr, pathBase)
 }
